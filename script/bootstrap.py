a022e393ab50b737d36d7e898b3becaca0b66682
stayHF <stay.hf@icloud.com>
2016-02-18 17:01:20 +0800

    modify bootstrap

--- script/bootstrap.py
+++ script/bootstrap.py
@@ -2,10 +2,9 @@
 
 import sys
 import os
-from util import execute_stdout
+import util
 
 SOURCE_ROOT = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
-VENDOR_DIR = os.path.join(os.path.dirname(SOURCE_ROOT), 'vendor')
 
 def main():
     os.chdir(SOURCE_ROOT)
@@ -14,11 +13,11 @@ def main():
     update_submodules()
 
 def update_clang():
-    execute_stdout([os.path.join(SOURCE_ROOT, 'script', 'update-clang.sh')])
+    util.execute_stdout([os.path.join('script', 'update-clang.sh')])
 
 def update_submodules():
-    execute_stdout(['git', 'submodule', 'sync'])
-    execute_stdout(['git', 'submodule', 'update', '--init', '--recursive'])
+    util.execute_stdout(['git', 'submodule', 'sync'])
+    util.execute_stdout(['git', 'submodule', 'update', '--init', '--recursive'])
 
 if __name__ == '__main__':
     sys.exit(main())
